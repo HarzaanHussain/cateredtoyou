a0dfc33afa2c6192f5779426174fe1d377e439d8
thomaschor <99774559+thomaschor@users.noreply.github.com>
2025-03-04 15:42:52 -0800

    Move notifications menu item to toolbar and remove it from the homepage. Need to create permission id for notifications in future, using generic for now.

--- lib/widgets/bottom_toolbar.dart
+++ lib/widgets/bottom_toolbar.dart
@@ -13,14 +13,6 @@ class BottomToolbar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           PermissionWidget(
-            permissionId: 'view_tasks',
-            child: IconButton(
-              icon: const Icon(Icons.task, size: 32),
-              tooltip: 'Tasks',
-              onPressed: () => context.push('/tasks'),
-            ),
-          ),
-          PermissionWidget(
             permissionId: 'manage_events',
             child: IconButton(
               icon: const Icon(Icons.event_available, size: 32),
@@ -39,6 +31,15 @@ class BottomToolbar extends StatelessWidget {
               icon: const Icon(Icons.route, size: 32),
               tooltip: 'My Deliveries',
               onPressed: () => context.push('/driver-deliveries'),
+            ),
+          ),
+          PermissionWidget(
+            // Need to create a notifications permission id, using placeholder now
+            permissionId: 'manage_menu',
+            child: IconButton(
+              icon: const Icon(Icons.notifications, size: 32),
+              tooltip: 'Notifications',
+              onPressed: () => context.push('/notifications'),
             ),
           ),
         ],
