00b33853b0f00d1c2f04882f2fe3b79cc0e9bd4e
thomaschor <99774559+thomaschor@users.noreply.github.com>
2025-03-27 16:51:36 -0700

    Change global button toggles to green/white

--- lib/main.dart
+++ lib/main.dart
@@ -148,6 +148,17 @@ class MyApp extends StatelessWidget {
                   seedColor: Colors.blue, // Set primary color to blue
                   brightness: Brightness.light, // Set brightness to light mode
                 ),
+              switchTheme: SwitchThemeData(
+                  thumbColor: MaterialStateProperty.resolveWith<Color>((states) {
+                    if (states.contains(MaterialState.selected)) {
+                      return Colors.white;  // Thumb color when the switch is active
+                    }
+                    return Colors.white;  // Thumb color when the switch is inactive
+                  }),
+                trackColor: MaterialStateProperty.all(Colors.green),  // Track color
+                // You can set different colors for active and inactive states
+                overlayColor: MaterialStateProperty.all(Colors.green.withOpacity(0.4)),  // Circle overlay color
+              ),
               primaryColor: const Color(0xFFFBC72B),
 
               useMaterial3: true, // Use Material 3 design
